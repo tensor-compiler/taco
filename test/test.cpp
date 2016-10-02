28cc5301965c0b2a7705887c98f1531a7c2e0b82
Fredrik Kjolstad <fred@csail.mit.edu>
2016-10-02 09:12:12 -0400

    Give taco-test simit-test arguments

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,24 @@
 #include "test.h"
 
 int main(int argc, char **argv) {
+  // If there is just one argument and it is not a gtest option, then filter
+  // the tests using that argument surrounded by wildcards.
+  std::string filter;
+  if (argc == 2 && std::string(argv[argc-1]).substr(0,2) != "--") {
+    filter = std::string(argv[1]);
+
+    char *dotPtr = strchr(argv[1], '.');
+    if (!dotPtr) {
+      filter = "*" + filter + "*";
+      }
+      else if (dotPtr[1] == '\0') {
+        filter = filter + "*";
+      }
+
+      filter = std::string("--gtest_filter=") + filter;
+      argv[1] = (char*)filter.c_str();
+  }
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
