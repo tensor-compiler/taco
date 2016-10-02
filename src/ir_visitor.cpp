bcb58266f4e617584619e93a7b5f42a2211e2594
Shoaib Kamil <skamil@mit.edu>
2016-10-01 20:33:52 -0400

    Missing files.

---
+++ src/ir_visitor.cpp
@@ -0,0 +1,121 @@
+#include "ir_visitor.h"
+
+namespace taco {
+namespace internal {
+
+IRVisitor::~IRVisitor() {
+}
+
+void IRVisitor::visit(const Literal*) {
+}
+
+void IRVisitor::visit(const Var*) {
+}
+
+void IRVisitor::visit(const Add* op) {
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Sub* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Mul* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Div* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Rem* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Min* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Max* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Eq* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Neq* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Gt* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Lt* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Gte* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Lte* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const And* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const Or* op){
+  op->a.accept(this);
+  op->b.accept(this);
+}
+
+void IRVisitor::visit(const IfThenElse* op) {
+  op->cond.accept(this);
+  op->then.accept(this);
+  op->otherwise.accept(this);
+}
+
+void IRVisitor::visit(const Load* op) {
+  op->arr.accept(this);
+  op->loc.accept(this);
+}
+
+void IRVisitor::visit(const Store* op) {
+  op->arr.accept(this);
+  op->loc.accept(this);
+  op->data.accept(this);
+}
+
+void IRVisitor::visit(const For* op) {
+  op->var.accept(this);
+  op->start.accept(this);
+  op->end.accept(this);
+  op->increment.accept(this);
+  op->contents.accept(this);
+}
+
+void IRVisitor::visit(const Block* op) {
+  for (auto s:op->contents)
+    s.accept(this);
+}
+
+}  // namespace taco
+}  // namespace internal
---
+++ src/ir_visitor.h
@@ -0,0 +1,41 @@
+#ifndef TAC_IR_VISITOR_H
+#define TAC_IR_VISITOR_H
+
+#include "ir.h"
+
+namespace taco {
+namespace internal {
+
+/** A base class for visitors to the IR.
+ */
+class IRVisitor {
+public:
+  virtual ~IRVisitor();
+  virtual void visit(const Literal*);
+  virtual void visit(const Var*);
+  virtual void visit(const Add*);
+  virtual void visit(const Sub*);
+  virtual void visit(const Mul*);
+  virtual void visit(const Div*);
+  virtual void visit(const Rem*);
+  virtual void visit(const Min*);
+  virtual void visit(const Max*);
+  virtual void visit(const Eq*);
+  virtual void visit(const Neq*);
+  virtual void visit(const Gt*);
+  virtual void visit(const Lt*);
+  virtual void visit(const Gte*);
+  virtual void visit(const Lte*);
+  virtual void visit(const And*);
+  virtual void visit(const Or*);
+  virtual void visit(const IfThenElse*);
+  virtual void visit(const Load*);
+  virtual void visit(const Store*);
+  virtual void visit(const For*);
+  virtual void visit(const Block*);
+
+};
+
+} // namespace internal
+} // namespace taco
+#endif
