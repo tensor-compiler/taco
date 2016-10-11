0e24baa4903345646c05dd680b1e6d132b9191be
Fredrik Kjolstad <fred@csail.mit.edu>
2016-10-11 08:51:44 -0400

    Adds collections utils

---
+++ src/util/collections.h
@@ -0,0 +1,211 @@
+#ifndef TACO_UTIL_COLLECTIONS_H
+#define TACO_UTIL_COLLECTIONS_H
+
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+
+#include "util/comparable.h"
+
+namespace taco {
+namespace util {
+
+/// Query whether a collection contains an element
+template <class C, typename V>
+bool contains(const C &container, const V &value) {
+  return std::find(container.begin(),container.end(),value) != container.end();
+}
+
+/// Query whether a set contains an element
+template <typename V>
+bool contains(const std::set<V> &container, const V &value) {
+  return container.find(value) != container.end();
+}
+
+/// Query whether a map contains an element
+template <typename K, typename V>
+bool contains(const std::map<K,V> &container, const K &key) {
+  return container.find(key) != container.end();
+}
+
+/// Query whether a set contains an element
+template <typename V>
+bool contains(const std::unordered_set<V> &container, const V &value) {
+  return container.find(value) != container.end();
+}
+
+/// Query whether a map contains an element
+template <typename K, typename V>
+bool contains(const std::unordered_map<K,V> &container, const K &key) {
+  return container.find(key) != container.end();
+}
+
+/// Query whether a collection is all zeros
+template <class C>
+bool isAllZeros(const C offsets) {
+  for (auto off : offsets) {
+    if (off != 0) return false;
+  }
+  return true;
+}
+
+/// Retrieve the location in the collection of the given value
+template <class Collection, typename Value>
+size_t locate(const Collection &collection, const Value &value) {
+  assert(util::contains(collection, value));
+  return std::distance(collection.begin(), std::find(collection.begin(),
+                                                     collection.end(), value));
+}
+
+/// Map the elements of a collection to a vector, using the function f.
+template <class Collection, typename ParamValue, typename ResultValue>
+std::vector<ResultValue> map(const Collection &collection,
+                             std::function<ResultValue(ParamValue)> f) {
+  std::vector<ResultValue> result;
+  result.resize(collection.size());
+  std::transform(collection.begin(), collection.end(), result.begin(), f);
+  return result;
+}
+
+// Iterables
+template <class Collection>
+class ReverseIterable {
+public:
+  typedef typename Collection::reverse_iterator reverse_iterator;
+  ReverseIterable(Collection &c) : c(c) {}
+  reverse_iterator begin() {return c.rbegin();}
+  reverse_iterator end() {return c.rend();}
+private:
+  Collection &c;
+};
+
+template <class Collection>
+class ReverseConstIterable {
+public:
+  typedef typename Collection::const_reverse_iterator const_reverse_iterator;
+  ReverseConstIterable(const Collection &c) : c(c) {}
+  const_reverse_iterator begin() const {return c.rbegin();}
+  const_reverse_iterator end() const {return c.rend();}
+private:
+  const Collection &c;
+};
+
+template <class Collection>
+class ExcludeFirstIterable {
+public:
+  typedef typename Collection::iterator iterator;
+  ExcludeFirstIterable(Collection &c) : c(c) {}
+  iterator begin() {return (c.begin() == c.end() ? c.end() : ++c.begin());}
+  iterator end() {return c.end();}
+private:
+  Collection &c;
+};
+
+template <class Collection>
+class ExcludeFirstConstIterable {
+public:
+  typedef typename Collection::const_iterator const_iterator;
+  ExcludeFirstConstIterable(const Collection &c) : c(c) {}
+  const_iterator begin() const {return c.begin()==c.end()?c.end():++c.begin();}
+  const_iterator end() const {return c.end();}
+private:
+  const Collection &c;
+};
+
+
+template <class C1, class C2>
+class ZipConstIterable {
+public:
+  typedef typename C1::value_type value_type1;
+  typedef typename C2::value_type value_type2;
+  struct pair {
+    const value_type1 &first;
+    const value_type2 &second;
+    pair(const value_type1 &first, const value_type2 &second)
+        : first(first), second(second){}
+    friend std::ostream& operator<<(std::ostream &os, const pair &p) {
+      return os << "(" << p.first << ", " << p.second << ")";
+    }
+  };
+  class ZipConstIterator {
+  public:
+    typedef typename C1::const_iterator const_iterator1;
+    typedef typename C2::const_iterator const_iterator2;
+    ZipConstIterator(const_iterator1 c1it, const_iterator2 c2it)
+        : c1it(c1it), c2it(c2it) {}
+    ZipConstIterator(const ZipConstIterator& zit)
+        : c1it(zit.c1it), c2it(zit.c2it) {}
+    ZipConstIterator& operator++() {
+      ++c1it;
+      ++c2it;
+      return *this;
+    }
+    ZipConstIterator operator++(int) {
+      ZipConstIterator tmp(*this);
+      operator++();
+      return tmp;
+    }
+    friend bool operator==(const ZipConstIterator &l,
+                           const ZipConstIterator &r) {
+      return l.c1it == r.c1it && l.c2it == r.c2it;
+    }
+    friend bool operator!=(const ZipConstIterator &l,
+                           const ZipConstIterator &r) {
+      return l.c1it != r.c1it || l.c2it != r.c2it;
+    }
+    pair operator*() {
+      return pair(*c1it, *c2it);
+    }
+
+  private:
+    const_iterator1 c1it;
+    const_iterator2 c2it;
+  };
+  ZipConstIterable(const C1 &c1, const C2 &c2) : c1(c1), c2(c2) {}
+  ZipConstIterator begin() const {
+    return ZipConstIterator(c1.begin(),c2.begin());
+  }
+  ZipConstIterator end() const {
+    return ZipConstIterator(c1.end(), c2.end());
+  }
+private:
+  const C1 &c1;
+  const C2 &c2;
+};
+
+/// Iterate over a collection in reverse using a range for loop:
+/// for (auto &element : util::reverse(collection)) {...}
+template <class C>
+ReverseIterable<C> reverse(C &collection) {
+  return ReverseIterable<C>(collection);
+}
+
+/// Iterate over a collection in reverse using a range for loop:
+/// for (auto &element : util::reverse(collection)) {...}
+template <class C>
+ReverseIterable<C> reverse(const C &collection) {
+  return ReverseConstIterable<C>(collection);
+}
+
+/// Iterate over the elements in a collection, excluding the first element.
+template <class C>
+ExcludeFirstIterable<C> excludeFirst(C &collection) {
+  return ExcludeFirstIterable<C>(collection);
+}
+
+/// Iterate over the elements in a collection, excluding the first element.
+template <class C>
+ExcludeFirstConstIterable<C> excludeFirst(const C &collection) {
+  return ExcludeFirstConstIterable<C>(collection);
+}
+
+/// Zip the iterators of two collections. If one collection is smaller, the zip
+/// iterates util there are no more elements in that collection.
+template <class C1, class C2>
+ZipConstIterable<C1, C2> zip(const C1 &collection1, const C2 &collection2) {
+  return ZipConstIterable<C1, C2>(collection1, collection2);
+}
+
+}}
+#endif
