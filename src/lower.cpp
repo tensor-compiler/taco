b8162a53939c79ec6ada27fcdc484503c55eaa9b
Fredrik Kjolstad <fred@csail.mit.edu>
2016-10-11 11:12:16 -0400

    Adds some lowering code

--- src/lower.cpp
+++ src/lower.cpp
@@ -4,6 +4,7 @@
 #include "expr.h"
 #include "operator.h"
 #include "iteration_schedule.h"
+#include "component_types.h"
 #include "ir.h"
 #include "util/strings.h"
 
@@ -12,11 +13,28 @@ using namespace std;
 namespace taco {
 namespace internal {
 
+Stmt lower(const IterationSchedule& schedule, size_t level) {
+  vector<Stmt> code;
+  iassert(level < schedule.getIndexVariables().size());
+
+  vector<taco::Var> vars  = schedule.getIndexVariables()[level];
+  for (taco::Var var : vars) {
+    Expr i = Var::make(var.getName(), typeOf<int>(), false);
+    Stmt loop = For::make(i, 0, 10, 1, Block::make());
+
+    code.push_back(loop);
+  }
+
+  return Block::make(code);
+}
+
 Stmt lower(const internal::Tensor& tensor, LowerKind lowerKind) {
-  taco::Expr expr = tensor.getExpr();
+  auto expr     = tensor.getExpr();
   auto schedule = IterationSchedule::make(expr);
 
-  Stmt body = Block::make();
+  // Lower the iteration schedule
+  Stmt body = lower(schedule, 0);
+  std::cout << body << std::endl;
 
   string funcName;
   switch (lowerKind) {
