d0500ad93a47bcf57415e9b4a93dd9c96b903851
Fredrik Kjolstad <fred@csail.mit.edu>
2016-10-11 08:52:07 -0400

    Adds elwise dense matrix test

--- test/elwise-tests.cpp
+++ test/elwise-tests.cpp
@@ -7,14 +7,16 @@
 #include "packed_tensor.h"
 #include "operator.h"
 
-TEST(elwise, add_vector_to_self_dense) {
+TEST(elwise, vector_add_dense) {
   Var i("i");
 
   Tensor<double> a({5}, "d");
   Tensor<double> b = vectord5a("d");
+  Tensor<double> c = vectord5a("d");
   b.pack();
+  c.pack();
 
-  a(i) = b(i) + b(i);
+  a(i) = b(i) + c(i);
 
   a.compile();
   a.assemble();
@@ -30,4 +32,35 @@ TEST(elwise, add_vector_to_self_dense) {
   ASSERT_EQ(0u, indices[0].size());
 
   ASSERT_VECTOR_EQ({0.0, 2.0, 0.0, 0.0, 4.0}, values);
+}
+
+TEST(elwise, matrix_adds_dense) {
+  Var i("i");
+  Var j("j");
+
+  Tensor<double> A({3, 3}, "dd");
+  Tensor<double> B = matrixd33a("dd");
+  Tensor<double> C = matrixd33a("dd");
+  B.pack();
+  C.pack();
+
+  A(i,j) = B(i,j) + C(i,j);
+
+  A.compile();
+  A.assemble();
+  A.evaluate();
+
+  auto Apack = A.getPackedTensor();
+  ASSERT_NE(nullptr, Apack);
+
+  auto& indices = Apack->getIndices();
+  auto& values  = Apack->getValues();
+
+  ASSERT_EQ(2u, indices.size());
+  ASSERT_EQ(0u, indices[1].size());
+  ASSERT_EQ(0u, indices[2].size());
+
+  ASSERT_VECTOR_EQ({{0, 1, 0,
+                     0, 0, 0,
+                     2, 0, 3}}, values);
 }
