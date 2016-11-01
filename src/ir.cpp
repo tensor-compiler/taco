31be936cb97612109c7f6ffeda73cfe77dfcc990
Shoaib Kamil <kamil@adobe.com>
2016-11-01 10:16:34 -0500

    Add is_decl field to VarAssign.  Unused by codegen.

--- src/ir.cpp
+++ src/ir.cpp
@@ -333,12 +333,13 @@ Stmt Function::make(std::string name, std::vector<Expr> inputs,
 }
 
 // VarAssign
-Stmt VarAssign::make(Expr lhs, Expr rhs) {
+Stmt VarAssign::make(Expr lhs, Expr rhs, bool is_decl) {
   iassert(lhs.as<Var>() || lhs.as<GetProperty>())
     << "Can only assign to a Var or GetProperty";
   VarAssign *assign = new VarAssign;
   assign->lhs = lhs;
   assign->rhs = rhs;
+  assign->is_decl = is_decl;
   return assign;
 }
 
