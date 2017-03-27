0d1a4c1301902f3dfc6f525f3f52dbd82a9b5811
Stephen Chou <s3chou@csail.mit.edu>
2017-03-27 14:36:48 -0400

    Implemented iterator over nonzeros for TensorBase

--- include/taco/tensor_base.h
+++ include/taco/tensor_base.h
@@ -11,6 +11,7 @@
 #include "taco/component_types.h"
 #include "taco/util/comparable.h"
 #include "taco/util/strings.h"
+#include "storage/storage.h"
 
 namespace taco {
 class Var;
@@ -79,6 +80,9 @@ public:
   /// Read a sparse matrix from a file stored in the MTX format.
   void readMTX(std::string filename);
 
+  /// Write a sparse matrix to a file stored in the MTX format.
+  void writeMTX(std::string filename) const;
+
   /// Pack tensor into the given format
   void pack();
 
@@ -106,9 +110,6 @@ public:
   friend bool operator!=(const TensorBase&, const TensorBase&);
   friend bool operator<(const TensorBase&, const TensorBase&);
 
-private:
-  struct Content;
-
   struct Coordinate : util::Comparable<Coordinate> {
     typedef std::vector<int> Coord;
 
@@ -117,7 +118,7 @@ private:
     Coordinate(const Coord& loc, double val) : loc(loc), dval(val) {}
     Coordinate(const Coord& loc, bool   val) : loc(loc), bval(val) {}
 
-    std::vector<int> loc;
+    Coord loc;
     union {
       int    ival;
       float  fval;
@@ -135,6 +136,180 @@ private:
     }
   };
   
+  class const_iterator {
+  public:
+    typedef const_iterator self_type;
+    typedef Coordinate value_type;
+    typedef Coordinate& reference;
+    typedef Coordinate* pointer;
+    typedef std::forward_iterator_tag iterator_category;
+
+    const_iterator(const const_iterator&) = default;
+
+    const_iterator operator++() {
+      advanceIndex();
+      return *this;
+    }
+
+    const Coordinate& operator*() const {
+      return curVal;
+    }
+
+    const Coordinate* operator->() const {
+      return &curVal;
+    }
+
+    bool operator==(const const_iterator& rhs) {
+      return tensor == rhs.tensor && count == rhs.count;
+    }
+
+    bool operator!=(const const_iterator& rhs) {
+      return !(*this == rhs);
+    }
+
+  private:
+    friend class TensorBase;
+
+    const_iterator(const TensorBase* tensor, bool isEnd = false) : 
+        tensor(tensor),
+        coord(Coordinate::Coord(tensor->getOrder())),
+        ptrs(Coordinate::Coord(tensor->getOrder())),
+        curVal(Coordinate(Coordinate::Coord(tensor->getOrder()), 0)),
+        count(1 + (size_t)isEnd * tensor->getStorage().getSize().values),
+        advance(false) {
+      advanceIndex();
+    }
+
+    void advanceIndex() {
+      advanceIndex(0);
+      ++count;
+    }
+
+    bool advanceIndex(size_t lvl) {
+      const auto& levels = tensor->getFormat().getLevels();
+
+      if (lvl == tensor->getOrder()) {
+        if (advance) {
+          advance = false;
+          return false;
+        }
+
+        const size_t idx = (lvl == 0) ? 0 : ptrs[lvl - 1];
+        switch (tensor->getComponentType().getKind()) {
+          case ComponentType::Bool:
+            curVal.bval = tensor->getStorage().getValues()[idx];
+            break;
+          case ComponentType::Int:
+            curVal.ival = tensor->getStorage().getValues()[idx];
+            break;
+          case ComponentType::Float:
+            curVal.fval = tensor->getStorage().getValues()[idx];
+            break;
+          case ComponentType::Double:
+            curVal.dval = tensor->getStorage().getValues()[idx];
+            break;
+          default:
+            taco_not_supported_yet;
+            break;
+        }
+
+        for (size_t i = 0; i < lvl; ++i) {
+          const size_t dim = levels[i].getDimension();
+          curVal.loc[dim] = coord[i];
+        }
+
+        advance = true;
+        return true;
+      }
+      
+      const auto storage    = tensor->getStorage();
+      const auto levelIndex = storage.getLevelIndex(lvl);
+
+      switch (levels[lvl].getType()) {
+        case Dense: {
+          const auto dim  = levelIndex.ptr[0];
+          const auto base = (lvl == 0) ? 0 : (ptrs[lvl - 1] * dim);
+
+          if (advance) {
+            goto resume_dense;  // obligatory xkcd: https://xkcd.com/292/
+          }
+
+          for (coord[lvl] = 0; coord[lvl] < dim; ++coord[lvl]) {
+            ptrs[lvl] = base + coord[lvl];
+
+          resume_dense:
+            if (advanceIndex(lvl + 1)) {
+              return true;
+            }
+          }
+          break;
+        }
+        case Sparse: {
+          const auto& segs = levelIndex.ptr;
+          const auto& vals = levelIndex.idx;
+          const auto  k    = (lvl == 0) ? 0 : ptrs[lvl - 1];
+
+          if (advance) {
+            goto resume_sparse;
+          }
+
+          for (ptrs[lvl] = segs[k]; ptrs[lvl] < segs[k + 1]; ++ptrs[lvl]) {
+            coord[lvl] = vals[ptrs[lvl]];
+
+          resume_sparse:
+            if (advanceIndex(lvl + 1)) {
+              return true;
+            }
+          }
+          break;
+        }
+        case Fixed: {
+          const auto  elems = levelIndex.ptr[0];
+          const auto  base  = (lvl == 0) ? 0 : (ptrs[lvl - 1] * elems);
+          const auto& vals  = levelIndex.idx;
+
+          if (advance) {
+            goto resume_fixed;
+          }
+
+          for (ptrs[lvl] = base; 
+               ptrs[lvl] < base + elems && vals[ptrs[lvl]] >= 0; ++ptrs[lvl]) {
+            coord[lvl] = vals[ptrs[lvl]];
+
+          resume_fixed:
+            if (advanceIndex(lvl + 1)) {
+              return true;
+            }
+          }
+          break;
+        }
+        default:
+          taco_not_supported_yet;
+          break;
+      }
+
+      return false;
+    }
+
+    const TensorBase* tensor;
+    Coordinate::Coord coord;
+    Coordinate::Coord ptrs;
+    Coordinate        curVal;
+    size_t            count;
+    bool              advance;
+  };
+
+  const_iterator begin() const {
+    return const_iterator(this);
+  }
+
+  const_iterator end() const {
+    return const_iterator(this, true);
+  }
+
+private:
+  struct Content;
+
   void compute(bool pack);
 
   friend std::ostream& operator<<(std::ostream&, const TensorBase&);
