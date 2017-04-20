a532e47e7660051299aa1f29c8fc5662fb61c288
Fredrik Kjolstad <fred@csail.mit.edu>
2017-04-20 09:36:01 -0400

    New Format constructor

--- include/taco/format.h
+++ include/taco/format.h
@@ -23,8 +23,11 @@ public:
   typedef std::vector<size_t>    DimensionOrders;
 
   Format();
-  Format(LevelTypes levelTypes, DimensionOrders dimensionOrder);
-  Format(LevelTypes levelTypes);
+  explicit Format(LevelTypes levelTypes);
+  explicit Format(LevelTypes levelTypes, DimensionOrders dimensionOrder);
+
+  /// Create a format where all levels are the same type
+  explicit Format(LevelType levelType, size_t numLevels);
 
   const std::vector<Level>& getLevels() const {return levels;}
 
