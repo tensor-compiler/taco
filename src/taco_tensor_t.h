de8d44f3f89a0f5ac62efbcba8aed157ebc4c3a5
Shoaib Kamil <kamil@adobe.com>
2017-04-24 14:19:45 -0400

    Add guards to taco_tensor_t definition

--- src/taco_tensor_t.h
+++ src/taco_tensor_t.h
@@ -1,6 +1,8 @@
 /// This file defines the runtime struct used to pass raw tensors to generated
 /// code.  Note: this file must be valid C99, not C++.
 
+#ifndef TACO_TENSOR_T_DEFINED
+#define TACO_TENSOR_T_DEFINED
 enum { taco_level_dense, taco_level_sparse } taco_level_t;
 
 typedef struct {
@@ -12,9 +14,10 @@ typedef struct {
                              // (the logical size of the corresponding dimension)
   
   int** pos;                // an array of pointers, each pointer points to a level's "pos" array
-  int** idxs;                // an array of pointers, each pointer points to a level's "indices" array
+  int** idx;                // an array of pointers, each pointer points to a level's "indices" array
   
   int elem_size;             // the size of an element, in bytes.  currently only 8 is supported (doubles)
   uint8_t* vals;             // a pointer to the values array
 
 } taco_tensor_t;
+#endif
