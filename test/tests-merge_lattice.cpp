46d6f12727b9ca4d5e4b951085699037b36b31bd
Fredrik Kjolstad <fred@csail.mit.edu>
2018-09-26 09:58:57 -0400

    Adds hashmap merge test class and some hash merge tests

--- test/tests-merge_lattice.cpp
+++ test/tests-merge_lattice.cpp
@@ -8,6 +8,7 @@
 #include "taco/lower/iterator.h"
 #include "lower/mode_access.h"
 #include "taco/ir/ir.h"
+#include "taco/lower/mode_format_impl.h"
 
 using namespace std;
 
@@ -17,6 +18,25 @@ using namespace taco;
 #include "taco/lower/mode_format_dense.h"
 taco::ModeFormat dense_new(std::make_shared<taco::DenseModeFormat>());
 #define dense dense_new
+
+namespace tests {
+
+class HashmapModeFormat : public ModeFormatImpl {
+public:
+  HashmapModeFormat() : ModeFormatImpl("hashmap", false, false,
+                                       true, false, false,
+                                       false, true, true,
+                                       true, false) {}
+
+  ModeFormat copy(std::vector<ModeFormat::Property> properties) const {
+    return ModeFormat(std::make_shared<HashmapModeFormat>());
+  }
+
+  vector<ir::Expr> getArrays(ir::Expr tensor, int mode) const {
+    return {};
+  }
+};
+ModeFormat hashed(make_shared<HashmapModeFormat>());
 
 static const Dimension n;
 static const Type vectype(Float64, {n});
@@ -28,10 +48,14 @@ static TensorVar d1t("d1", vectype, Format(dense));
 static TensorVar d2t("d2", vectype, Format(dense));
 static TensorVar d3t("d3", vectype, Format(dense));
 static TensorVar d4t("d4", vectype, Format(dense));
-static TensorVar s1t("s1", vectype, Format(sparse));
-static TensorVar s2t("s2", vectype, Format(sparse));
-static TensorVar s3t("s3", vectype, Format(sparse));
-static TensorVar s4t("s4", vectype, Format(sparse));
+static TensorVar s1t("s1", vectype, Format(compressed));
+static TensorVar s2t("s2", vectype, Format(compressed));
+static TensorVar s3t("s3", vectype, Format(compressed));
+static TensorVar s4t("s4", vectype, Format(compressed));
+static TensorVar h1t("h1", vectype, Format(hashed));
+static TensorVar h2t("h2", vectype, Format(hashed));
+static TensorVar h3t("h3", vectype, Format(hashed));
+static TensorVar h4t("h4", vectype, Format(hashed));
 
 static map<TensorVar, taco::ir::Expr> tensorVars {
   {r1t, taco::ir::Var::make("r1", taco::Int())},
@@ -44,6 +68,10 @@ static map<TensorVar, taco::ir::Expr> tensorVars {
   {s2t, taco::ir::Var::make("s2", taco::Int())},
   {s3t, taco::ir::Var::make("s3", taco::Int())},
   {s4t, taco::ir::Var::make("s4", taco::Int())},
+  {h1t, taco::ir::Var::make("h1", taco::Int())},
+  {h2t, taco::ir::Var::make("h2", taco::Int())},
+  {h3t, taco::ir::Var::make("h3", taco::Int())},
+  {h4t, taco::ir::Var::make("h4", taco::Int())}
 };
 
 static IndexVar i("i");
@@ -58,10 +86,17 @@ static Access s1 = s1t(i);
 static Access s2 = s2t(i);
 static Access s3 = s3t(i);
 static Access s4 = s4t(i);
+static Access h1 = h1t(i);
+static Access h2 = h2t(i);
+static Access h3 = h3t(i);
+static Access h4 = h4t(i);
 
 static map<Iterator, IndexVar> indexVars;
 static map<IndexVar, taco::ir::Expr> coordVars;
-static Forall dummy = forall(i, rd = rd + d1 + d2 + d3 + d4 + s1 + s2 + s3 + s4);
+static Forall dummy = forall(i, rd = rd + rs +
+                                     d1 + d2 + d3 + d4 +
+                                     s1 + s2 + s3 + s4 +
+                                     h1 + h2 + h3 + h4);
 static map<ModeAccess, Iterator> iterators = createIterators(dummy, tensorVars,
                                                              &indexVars,
                                                              &coordVars);
@@ -71,10 +106,6 @@ static Iterator it(Access access) {
       << "Could not find " << ModeAccess(access, 1);
   return iterators.at(ModeAccess(access, 1));
 }
-
-
-
-namespace tests {
 
 struct Test {
   Test(Forall forall, taco::MergeLattice expected)
@@ -94,10 +125,26 @@ TEST_P(merge_lattice, test) {
   ASSERT_EQ(GetParam().expected, lattice);
 }
 
-INSTANTIATE_TEST_CASE_P(vector_neg, merge_lattice,
-  Values(Test(forall(i, rd = -d2),
+INSTANTIATE_TEST_CASE_P(copy, merge_lattice,
+  Values(Test(forall(i, rd = d1),
               MergeLattice({MergePoint({i},
-                                       {it(d2)},
+                                       {it(d1)},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = s1),
+              MergeLattice({MergePoint({it(s1)},
+                                       {},
+                                       {it(rd)})
+                           })
+              )
+         )
+);
+
+INSTANTIATE_TEST_CASE_P(neg, merge_lattice,
+  Values(Test(forall(i, rd = -d1),
+              MergeLattice({MergePoint({i},
+                                       {it(d1)},
                                        {it(rd)})
                            })
               ),
@@ -111,7 +158,7 @@ INSTANTIATE_TEST_CASE_P(vector_neg, merge_lattice,
 );
 
 
-INSTANTIATE_TEST_CASE_P(vector_mul, merge_lattice,
+INSTANTIATE_TEST_CASE_P(mul, merge_lattice,
   Values(Test(forall(i, rd = d1 * d2),
               MergeLattice({MergePoint({i},
                                        {it(d1), it(d2)},
@@ -181,7 +228,7 @@ INSTANTIATE_TEST_CASE_P(vector_mul, merge_lattice,
         )
 );
 
-INSTANTIATE_TEST_CASE_P(vector_add, merge_lattice,
+INSTANTIATE_TEST_CASE_P(add, merge_lattice,
   Values(Test(forall(i, rd = d1 + d2),
               MergeLattice({MergePoint({i},
                                        {it(d1), it(d2)},
@@ -251,7 +298,7 @@ INSTANTIATE_TEST_CASE_P(vector_add, merge_lattice,
         )
 );
 
-INSTANTIATE_TEST_CASE_P(vector_add_multiply, merge_lattice,
+INSTANTIATE_TEST_CASE_P(add_multiply, merge_lattice,
   Values(Test(forall(i, rd = (d1 + d2) * d3),
               MergeLattice({MergePoint({i},
                                        {it(d1), it(d2), it(d3)},
@@ -300,7 +347,7 @@ INSTANTIATE_TEST_CASE_P(vector_add_multiply, merge_lattice,
         )
 );
 
-INSTANTIATE_TEST_CASE_P(vector_multiply_add, merge_lattice,
+INSTANTIATE_TEST_CASE_P(multiply_add, merge_lattice,
   Values(Test(forall(i, rd = (d1 * d2) + d3),
               MergeLattice({MergePoint({i},
                                        {it(d1), it(d2), it(d3)},
@@ -349,7 +396,7 @@ INSTANTIATE_TEST_CASE_P(vector_multiply_add, merge_lattice,
         )
 );
 
-INSTANTIATE_TEST_CASE_P(vector_add_multiply_multiply, merge_lattice,
+INSTANTIATE_TEST_CASE_P(add_multiply_multiply, merge_lattice,
   Values(Test(forall(i, rd = (d1 + d2) * (d3 * d4)),
               MergeLattice({MergePoint({i},
                                        {it(d1), it(d2), it(d3), it(d4)},
@@ -398,7 +445,7 @@ INSTANTIATE_TEST_CASE_P(vector_add_multiply_multiply, merge_lattice,
         )
 );
 
-INSTANTIATE_TEST_CASE_P(vector_add_multiply_add, merge_lattice,
+INSTANTIATE_TEST_CASE_P(add_multiply_add, merge_lattice,
   Values(Test(forall(i, rd = (d1 + d2) * (d3 + d4)),
               MergeLattice({MergePoint({i},
                                        {it(d1), it(d2), it(d3), it(d4)},
@@ -468,5 +515,50 @@ INSTANTIATE_TEST_CASE_P(vector_add_multiply_add, merge_lattice,
         )
 );
 
+INSTANTIATE_TEST_CASE_P(hashmap, merge_lattice,
+  Values(Test(forall(i, rd = h1),
+              MergeLattice({MergePoint({it(h1)},
+                                       {},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = -h1),
+              MergeLattice({MergePoint({it(h1)},
+                                       {},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = d1 * h1),
+              MergeLattice({MergePoint({it(h1)},
+                                       {it(d1)},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = h1 * d1),
+              MergeLattice({MergePoint({it(h1)},
+                                       {it(d1)},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = s1 * h1),
+              MergeLattice({MergePoint({it(s1)},
+                                       {it(h1)},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = h1 * s1),
+              MergeLattice({MergePoint({it(s1)},
+                                       {it(h1)},
+                                       {it(rd)})
+                           })
+              ),
+         Test(forall(i, rd = h1 * h2),
+              MergeLattice({MergePoint({it(h1)},
+                                       {it(h2)},
+                                       {it(rd)})
+                           })
+              )
+        )
+);
 
 }
