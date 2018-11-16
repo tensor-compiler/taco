8a10feaefe27743321c798e525a367025f730ca1
Ryan Senanayake <rsen@mit.edu>
2018-11-16 00:38:31 -0500

    added unified memory, running tests now

--- include/taco/taco_tensor_t.h
+++ include/taco/taco_tensor_t.h
@@ -5,6 +5,8 @@
 #ifndef TACO_TENSOR_T_DEFINED
 #define TACO_TENSOR_T_DEFINED
 
+#include <cstdint>
+
 typedef enum { taco_mode_dense, taco_mode_sparse } taco_mode_t;
 
 typedef struct taco_tensor_t {
@@ -18,40 +20,10 @@ typedef struct taco_tensor_t {
   int32_t      vals_size;     // values array size
 } taco_tensor_t;
 
-static inline
 void init_taco_tensor_t(taco_tensor_t* t, int32_t order, int32_t csize,
                         int32_t* dimensions, int32_t* modeOrdering,
-                        taco_mode_t* mode_types) {
-  t->order         = order;
-  t->dimensions    =     (int32_t*)malloc(order * sizeof(int32_t));
-  t->mode_ordering =     (int32_t*)malloc(order * sizeof(int32_t));
-  t->mode_types    = (taco_mode_t*)malloc(order * sizeof(taco_mode_t));
-  t->indices       =   (uint8_t***)malloc(order * sizeof(uint8_t***));
-  t->csize         = csize;
+                        taco_mode_t* mode_types);
 
-  for (int32_t i = 0; i < order; i++) {
-    t->dimensions[i]    = dimensions[i];
-    t->mode_ordering[i] = modeOrdering[i];
-    t->mode_types[i]    = mode_types[i];
-    switch (t->mode_types[i]) {
-      case taco_mode_dense:
-        t->indices[i]    = (uint8_t**)malloc(1 * sizeof(uint8_t**));
-      case taco_mode_sparse:
-        t->indices[i]    = (uint8_t**)malloc(2 * sizeof(uint8_t**));
-    }
-  }
-}
-
-static inline
-void deinit_taco_tensor_t(taco_tensor_t* t) {
-  for (int i = 0; i < t->order; i++) {
-    free(t->indices[i]);
-  }
-  free(t->indices);
-
-  free(t->dimensions);
-  free(t->mode_ordering);
-  free(t->mode_types);
-}
+void deinit_taco_tensor_t(taco_tensor_t* t);
 
 #endif
