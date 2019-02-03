68274d0961fef995f27e2bd04131946ada5b5bd1
Stephen Chou <s3chou@csail.mit.edu>
2019-02-03 16:59:00 -0500

    Made copy propagation more precise

--- test/tests-copyprop.cpp
+++ test/tests-copyprop.cpp
@@ -5,10 +5,12 @@
 
 using taco::ir::Var;
 using taco::ir::VarDecl;
+using taco::ir::Assign;
 using taco::ir::Block;
 using taco::Int32;
 using taco::ir::simplify;
 using taco::ir::Neg;
+using taco::ir::Add;
 
 TEST(expr, simplify_copy) {
   auto a = Var::make("a", Int32), 
@@ -20,13 +22,14 @@ TEST(expr, simplify_copy) {
        bDecl = VarDecl::make(b, a),
        cDecl = VarDecl::make(c, b),
        // assign `d' a non-trivial expression so it won't get optimized away
-       dDecl = VarDecl::make(d, Neg::make(c));
-  auto block = Block::make({aDecl, bDecl, cDecl, dDecl});
+       dDecl = VarDecl::make(d, Neg::make(c)),
+       dInc = Assign::make(d, Add::make(d, 1));
+  auto block = Block::make({aDecl, bDecl, cDecl, dDecl, dInc});
 
   auto simplified = simplify(block);
   auto *simplifiedBlock = simplified.as<Block>();
-  ASSERT_EQ(simplifiedBlock->contents.size(), size_t(2));
+  ASSERT_EQ(simplifiedBlock->contents.size(), size_t(3));
 
-  const VarDecl *simplifiedDDecl = simplifiedBlock->contents.back().as<VarDecl>();
+  const VarDecl *simplifiedDDecl = simplifiedBlock->contents[1].as<VarDecl>();
   ASSERT_EQ(simplifiedDDecl->rhs.as<Neg>()->a, a);
 }
