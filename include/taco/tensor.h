5e97855f8a4ca4102a8f28907ae8ae24b6029032
patricion <patricionoyola@gmail.com>
2019-02-27 17:43:47 -0500

    Automated compiler methods Checkpoint

--- include/taco/tensor.h
+++ include/taco/tensor.h
@@ -422,6 +422,19 @@ public:
   /// Compile, assemble and compute as needed.
   void evaluate();
 
+  /// True if the Tensor needs to be packed.
+  inline bool needsPack();
+
+  /// True if the Tensor needs to be compiled.
+  inline bool needsCompile();
+
+  /// True if the Tensor needs to be assembled.
+  inline bool needsAssemble();
+
+  /// True if the Tensor needs to be computed.
+  inline bool needsCompute();
+
+
   /// Get the source code of the kernel functions.
   std::string getSource() const;
 
@@ -473,6 +486,18 @@ protected:
       const Format& format, Datatype ctype, const std::vector<int>& dimensions);
 
 private:
+  /* --- Compiler Methods    --- */
+  inline void setNeedsPack(bool needsPack);
+  inline void setNeedsCompile(bool needsCompile);
+  inline void setNeedsAssemble(bool needsAssemble);
+  inline void setNeedsCompute(bool needsCompute);
+
+  void addDependentTensor(TensorBase tensor);
+  std::vector<TensorBase> getDependentTensors();
+  void notifyDependentTensors();
+
+  void syncValues();
+
   struct Content;
   std::shared_ptr<Content> content;
 
