ce12b2b251ddecbac22692e82e5264679f87e210
patricion <patricionoyola@gmail.com>
2019-05-21 17:20:39 -0400

    test

--- apps/tensor_times_vector/tensor_times_vector.cpp
+++ apps/tensor_times_vector/tensor_times_vector.cpp
@@ -4,25 +4,100 @@
 using namespace taco;
 
 int main(int argc, char* argv[]) {
-  // Create formats
   Format csr({Dense,Sparse});
   Format csf({Sparse,Sparse,Sparse});
   Format  sv({Sparse});
 
-  // Create tensors
-  Tensor<double> A({2,3},   csr);
-  Tensor<double> B({2,3,4}, csf);
-  Tensor<double> c({4},     sv);
+  Tensor<double> A("A", {2,3},   csr);
+  Tensor<double> B("B", {2,3,4}, csf);
+  Tensor<double> c("c", {4},     sv);
 
-  // Insert data into B and c
   B(0,0,0) = 1.0;
   B(1,2,0) = 2.0;
   B(1,2,1) = 3.0;
   c(0) = 4.0;
   c(1) = 5.0;
 
-  // Form a tensor-vector multiplication expression
+  std::cout << "B.needsPack = " << B.needsPack() << std::endl;
+  std::cout << "c.needsPack = " << c.needsPack() << std::endl;
+
   IndexVar i, j, k;
   A(i,j) = B(i,j,k) * c(k);
-  std::cout << A << std::endl;
+
+  std::cout << "A(i,j) = B(i,j,k) * c(k);" << std::endl;
+
+  std::cout << "B.needsPack = " << B.needsPack() << std::endl;
+  std::cout << "c.needsPack = " << c.needsPack() << std::endl;
+
+  std::cout << "A.needsCompile = " << A.needsCompile() << std::endl;
+  std::cout << "A.needsCompute = " << A.needsCompute() << std::endl;
+
+  std::cout << "A.getDependentTensors" << std::endl;
+
+  for (auto t : A.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "B.getDependentTensors" << std::endl;
+
+  for (auto t : B.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "c.getDependentTensors" << std::endl;
+
+  for (auto t : c.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  // Modify on operand of A
+  c(0) = 1.0;
+  std::cout << "c(0) = 1.0;" << std::endl;
+
+  std::cout << "A.needsCompile = " << A.needsCompile() << std::endl;
+  std::cout << "A.needsCompute = " << A.needsCompute() << std::endl;
+
+  std::cout << "B.needsPack = " << B.needsPack() << std::endl;
+  std::cout << "c.needsPack = " << c.needsPack() << std::endl;
+
+  std::cout << "A.getDependentTensors" << std::endl;
+
+  for (auto t : A.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "B.getDependentTensors" << std::endl;
+
+  for (auto t : B.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "c.getDependentTensors" << std::endl;
+
+  for (auto t : c.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "print values" << std::endl;
+  for (auto val = A.beginTyped<int>(); val != A.endTyped<int>(); ++val) {
+    std::cout << val->second << std::endl;
+  }
+
+  std::cout << "A.getDependentTensors" << std::endl;
+
+  for (auto t : A.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "B.getDependentTensors" << std::endl;
+
+  for (auto t : B.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
+
+  std::cout << "c.getDependentTensors" << std::endl;
+
+  for (auto t : c.getDependentTensors()) {
+    std::cout << t.getName() << std::endl;
+  }
 }
