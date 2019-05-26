eae17721d309703c11c547e80ccc831b3d0d587d
Rawn <rawnhenry@gmail.com>
2019-05-26 00:47:23 -0400

    Added docs to test rendering

--- python_bindings/pytaco/pytensor/tensorIO.py
+++ python_bindings/pytaco/pytensor/tensorIO.py
@@ -3,9 +3,27 @@ from ..pytensor.taco_tensor import tensor
 
 
 def write(filename, t):
+    """
+    Writes a tensor to a file.
+
+    Parameters
+    -------------
+
+    Return
+    -------------
+    """
     _cm._write(filename, t._tensor)
 
 
 def read(filename, fmt, pack=True):
+    """
+    Writes a tensor to a file.
+
+    Parameters
+    -------------
+
+    Return
+    -------------
+    """
     cppTensor = _cm._read(filename, fmt, pack)
     return tensor._fromCppTensor(cppTensor)
