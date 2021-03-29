97d463e2266f7cc1d3a217e0e64ab41fdde916a3
Advay Pal <advaypal@hotmail.com>
2021-03-28 20:29:00 -0700

    new test

--- test/tests-blocking.cpp
+++ test/tests-blocking.cpp
@@ -10,18 +10,18 @@
 
 using namespace taco;
 
-TEST(blocking, test_dense_matrix_vector_mult) {
+TEST(blocking, test_dense_matrix_matrix_mult) {
+ 
+  Tensor<float> A("A", {{8, 8}, {4, 4}},
+                   Format({{Dense, Dense}, {Dense, Dense}}));
+  Tensor<float> B("B", {{8, 8}, {4, 4}},
+                   Format({{Dense, Dense}, {Dense, Dense}}));
+  Tensor<float> C("C", {{8, 8}, {4, 4}},
+                   Format({{Dense, Dense}, {Dense, Dense}}));
 
-  // Tensor<double> A("A", {{8, 8}, {4, 4}},
-  //                  Format({{Dense, Dense}, {Dense, Dense}}));
-  // Tensor<double> B("B", {{8, 8}, {4, 4}},
-  //                  Format({{Dense, Dense}, {Dense, Dense}}));
-  // Tensor<double> C("C", {{8, 8}, {4, 4}},
-  //                  Format({{Dense, Dense}, {Dense, Dense}}));
-
-  Tensor<float> B("B", {8, 8, 4, 4},
+  Tensor<float> B1("B1", {8, 8, 4, 4},
                    Format({Dense, Dense, Dense, Dense}));
-  Tensor<float> C("C", {8, 8, 4, 4},
+  Tensor<float> C1("C1", {8, 8, 4, 4},
                    Format({Dense, Dense, Dense, Dense}));
 
   for (int i0 = 0; i0 < 8; i0++) {
@@ -29,28 +29,46 @@ TEST(blocking, test_dense_matrix_vector_mult) {
       for (int i1 = 0; i1 < 4; i1++) {
         for (int j1 = 0; j1 < 4; j1++) {
           float rand_float = (float)rand()/(float)(RAND_MAX);
-          C.insert({i0, j0, i1, j1}, rand_float);
+          C1.insert({i0, j0, i1, j1}, rand_float);
+          C.insert({{i0, j0}, {i1, j1}}, rand_float);
           // TODO: also support inserting matrices?
           rand_float = (float)rand()/(float)(RAND_MAX);
-          B.insert({i0, j0, i1, j1}, rand_float);
+          B1.insert({i0, j0, i1, j1}, rand_float);
+          B.insert({{i0, j0}, {i1, j1}}, rand_float);
         }
       }
     }
   }
 
-  // A.pack();
+  A.pack();
   B.pack();
   C.pack();
+  B1.pack();
+  C1.pack();
 
-  // A(i0, k0) = B(i0, j0) * C(j0, k0);
-
-  IndexVar i0("i0"), i1("i1"), j0("j0"), j1("j1"), k0("k0");
+  IndexVar i0("i0"), j0("j0"), k0("k0");
+  A(i0, k0) = B(i0, j0) * C(j0, k0);
+  A.compile();
+  A.assemble();
+  A.compute();
+  
+  IndexVar i1("i1"), i2("i2"), j1("j1"), j2("j2"), k1("k1");
   Tensor<float> expected("expected", {8, 8, 4, 4},
                           Format({{Dense, Dense}, {Dense, Dense}}));
 
-  expected(i0, k0, i1, j1) = B(i0, j0, i1, j1) * C(j0, k0, i1, j1);
+  expected(i1, k1, i2, j2) = B1(i1, j1, i2, j2) * C1(j1, k1, i2, j2);
   expected.compile();
   expected.assemble();
   expected.compute();
-  ASSERT_TENSOR_EQ(expected, expected);
+
+  for (int i0 = 0; i0 < 8; i0++) {
+    for (int j0 = 0; j0 < 8; j0++) {
+      Tensor<float> blocked_matrix = A(i0, j0);
+      for (int i1 = 0; i1 < 4; i1++) {
+        for (int j1 = 0; j1 < 4; j1++) {
+          ASSERT_TRUE(expected(i0, j0, i1, j1) == blocked_matrix(i1, j1));
+        }
+      }
+    }
+  }
 }
