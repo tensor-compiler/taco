62cf2f3c17c07e9e9f7f261b4fca005b5bfe1cc3
Rohan Yadav <rohany@alumni.cmu.edu>
2021-04-30 14:07:52 -0700

    *: unpack tensor regions within tasks

--- src/codegen/codegen_legion_c.cpp
+++ src/codegen/codegen_legion_c.cpp
@@ -128,9 +128,9 @@ std::string CodegenLegionC::unpackTensorProperty(std::string varname, const GetP
     tp = "auto";
     ret << tp << " " << varname << " = " << tensor->name << ".get_index_space();\n";
   } else if (op->property == TensorProperty::ValuesReadAccessor) {
-    ret << "AccessorRO" << printType(op->type, false) << " " << varname << "(regions[?], FID_VAL);\n";
+    ret << "AccessorRO" << printType(op->type, false) << " " << varname << "(" << tensor->name << ", FID_VAL);\n";
   } else if (op->property == TensorProperty::ValuesWriteAccessor) {
-    ret << "AccessorRW" << printType(op->type, false) << " " << varname << "(regions[?], FID_VAL);\n";
+    ret << "AccessorRW" << printType(op->type, false) << " " << varname << "(" << tensor->name << ", FID_VAL);\n";
   } else {
     return CodeGen::unpackTensorProperty(varname, op, is_output_prop);
   }
