3cc4dfe84cabf7098abcb6f0ed122c953e9f3911
Rohan Yadav <rohany@alumni.cmu.edu>
2021-05-25 11:27:31 -0700

    legion: make using the logging mapper a compile time flag

--- legion/src/taco_mapper.cpp
+++ legion/src/taco_mapper.cpp
@@ -5,6 +5,10 @@ using namespace Legion;
 
 void register_taco_mapper(Machine machine, Runtime *runtime, const std::set<Processor> &local_procs) {
   for (auto it : local_procs) {
+#ifdef TACO_USE_LOGGING_MAPPER
     runtime->replace_default_mapper(new Mapping::LoggingWrapper(new TACOMapper(runtime->get_mapper_runtime(), machine, it)), it);
+#else
+    runtime->replace_default_mapper(new TACOMapper(runtime->get_mapper_runtime(), machine, it), it);
+#endif
   }
 }
