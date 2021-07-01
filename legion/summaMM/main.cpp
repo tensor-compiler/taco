f0daf2b524cdb57c640aeb69fa2511510c50422f
Rohan Yadav <rohany@alumni.cmu.edu>
2021-07-01 10:52:14 -0700

    legion: add flop tracking to SUMMA

--- legion/summaMM/main.cpp
+++ legion/summaMM/main.cpp
@@ -61,6 +61,7 @@ void top_level_task(const Task* task, const std::vector<PhysicalRegion>& regions
   auto bPart = partitionLegion(ctx, runtime, B, gx, gy);
   auto cPart = partitionLegion(ctx, runtime, C, gx, gy);
 
+  std::vector<size_t> times;
   // Run the benchmark several times.
   for (int i = 0; i < 10; i++) {
     tacoFill<valType>(ctx, runtime, A, aPart,0);
@@ -73,8 +74,15 @@ void top_level_task(const Task* task, const std::vector<PhysicalRegion>& regions
     placeLegionC(ctx, runtime, C, gx, gy);
 
     // Compute on the tensors.
-    benchmark(ctx, runtime, [&]() { computeLegion(ctx, runtime, A, B, C, part, gx); });
+    benchmark(ctx, runtime, times, [&]() { computeLegion(ctx, runtime, A, B, C, part, gx); });
   }
+
+  // Get the GFLOPS per node.
+  auto avgTime = average(times);
+  auto flopCount = getGEMMFLOPCount(n, n, n);
+  auto gflops = getGFLOPS(flopCount, avgTime);
+  auto nodes = runtime->select_tunable_value(ctx, Mapping::DefaultMapper::DEFAULT_TUNABLE_NODE_COUNT).get<size_t>();
+  LEGION_PRINT_ONCE(runtime, ctx, stdout, "On %ld nodes achieved GFLOPS per node: %lf.\n", nodes, gflops / double(nodes));
 
   tacoValidate<valType>(ctx, runtime, A, aPart, valType(n));
 }
